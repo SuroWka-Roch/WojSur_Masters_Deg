38711245bf70ca46263a611bf455e5a44cec843b
piernik <wojtek@surowka.com>
2019-07-11 18:35:51 +0200

    dokończ

--- magisterka.ino
+++ magisterka.ino
@@ -1,30 +1,11 @@
-#ifndef _SAM3XA_
-#define _SAM3XA_
 
-#if defined (__SAM3A4C__)
-#include "sam3a4c.h"
-#elif defined (__SAM3A8C__)
-#include "sam3a8c.h"
-#elif defined (__SAM3X4C__)
-#include "sam3x4c.h"
-#elif defined (__SAM3X4E__)
-#include "sam3x4e.h"
-#elif defined (__SAM3X8C__)
-#include "sam3x8c.h"
-#elif defined (__SAM3X8E__)
-#include "sam3x8e.h"
-#elif defined (__SAM3X8H__)
-#include "sam3x8h.h"
-#endif
 
-#endif /* _SAM3XA_ */
-
-#define SYSCLC *(volatile uint32_t *) 0x400E0610 // I can't find the clc makro - Register for perifrel clc control
+//#define SYSCLC *(volatile uint32_t *) 0x400E0610 // I can't find the clc makro - Register for perifrel clc control
 #define LOAD *(volatile uint32_t *) 0xE000E014 // Register defining value that system procesor loads when restarted - gives timer of milisecunds WO max 16777215
 #define CTRL *(volatile uint32_t *) 0xE000E018 // At 16 there is a flag wheter clc reached 0 here are adidional options. RW
 #define RELOAD *(volatile uint32_t *) 0xE000E018 // Writing anything in this register sets it to 0 sets TIMER_FLAG to 0 so it loads LOAD value.
-#define PIOC_OWDR *(volatile uint32_t *) 0x400E12A4 // Protect lines from writes on PIOC_ODSR, kind of a mask.
-#define PIOC_ODSR *(volatile uint32_t *) 0x400E1238 // Cool write register writen value is put on IO line
+//#define PIOC_OWDR *(volatile uint32_t *) 0x400E12A4 // Protect lines from writes on PIOC_ODSR, kind of a mask.
+//#define PIOC_ODSR *(volatile uint32_t *) 0x400E1238 // Cool write register writen value is put on IO line
 
 /**************************************************************************************/
 
@@ -46,46 +27,54 @@ void setup() {
 
   /* Using pointers to table as a way to deal witch multiplexing with low procesor yeald */
 
-  noInterrupts();
+  //noInterrupts();
   counts = 0;
   pinMode(33, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(33), add, RISING);
+  //attachInterrupt(digitalPinToInterrupt(33), add, RISING);
   
-  REG_PIOC_OER = pins; // enable output
+  //REG_PIOC_OER = pins; // enable output
   //REG_PIOC_PUDR = pins; // pull up disable
-  SYSCLC = 1 << 13; //PMC Peripheral Clock Enable Register 0, turn on a periferal clock
+  //SYSCLC = 1 << 13; //PMC Peripheral Clock Enable Register 0, turn on a periferal clock
   LOAD = ACQUISITION_TIME_MS;
 }
 
 char string[30]={0};
 long int a =0;
 void loop() {
-    Serial.print("lol");
+    flag=true;
+    Serial.println("lol");
+    delay(1000);
     char incomingByte = 0;
     if (Serial.available() > 0) {
       Serial.print("erp");
+      delay(100);
       incomingByte = Serial.read();
       if (incomingByte == 'A')
-      {
-        Serial.read();
-        Serial.read();
+      { 
+        While(flag){
+          if(Serial.available() > 0){
+            incomingByte = Serial.read();
+           }
+        }
         int i=0;
-        char b;
-        while(b=Serial.read()!=-1){
+        int b;
+        while((b=Serial.read())!=-1){
           string[i]=b;
+
           i++;
         }
         string[i]= '\0';
+        Serial.println(string);
         ACQUISITION_TIME_MS = String(string).toInt();
         LOAD = ACQUISITION_TIME_MS;
         Serial.println(ACQUISITION_TIME_MS);
         RELOAD = true; //start the clock TIMER_FLAG
         interrupts();
-       // while (!TIMER_FLAG) {
+        while (!TIMER_FLAG) {
           
-       // a++;
-    
-   //     }
+        a++;
+        Serial.println(ACQUISITION_TIME_MS);
+        }
         noInterrupts();
   
       }
