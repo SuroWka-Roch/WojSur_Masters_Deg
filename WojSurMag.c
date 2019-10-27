4c389c560a85b076804d2670c05e5616d4e39d04
piernik <wojtek@surowka.com>
2019-10-27 23:34:12 +0100

    Removed mistake with double ||

--- WojSurMag.c
+++ WojSurMag.c
@@ -3,13 +3,6 @@
 
 #define ALL 0xFFFFFFFF
 
-#if false
-#ifndef __NO_SYSTEM_INIT
-void SystemInit()
-{
-}
-#endif
-#endif
 int flag = 0;
 
 void dummy(void)
@@ -32,19 +25,19 @@ void set_up(void)
     REG_PMC_WPMR = 0x504D4300; //disable write protection
 
 
-    REG_PMC_PCER0 = REG_PMC_PCER0 || 0x1u << ID_PIOC; // Enable periferal clc
+    REG_PMC_PCER0 = REG_PMC_PCER0 | 0x1u << ID_PIOC; // Enable periferal clc
     REG_PIOC_PER = ALL;
     REG_PIOC_OER = ALL;
 
-    REG_PMC_PCER0 = REG_PMC_PCER0 || 0x1u << ID_PIOB; // Enable periferal clc
-    REG_PIOB_PER = REG_PIOB_PER || PIO_SODR_P27;
-    REG_PIOB_OER = REG_PIOB_OER || PIO_SODR_P27;
+    REG_PMC_PCER0 = REG_PMC_PCER0 | 0x1u << ID_PIOB; // Enable periferal clc
+    REG_PIOB_PER = REG_PIOB_PER | PIO_SODR_P27;
+    REG_PIOB_OER = REG_PIOB_OER | PIO_SODR_P27;
 }
 void main()
 {   
     set_up();
     REG_PIOC_SODR = ALL;
-    REG_PIOB_SODR = REG_PIOB_SODR || PIO_SODR_P27;
+    REG_PIOB_SODR = REG_PIOB_SODR | PIO_SODR_P27;
     for (;;)
         ;
     dummy();
