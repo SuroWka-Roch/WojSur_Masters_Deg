e4780afd87e74b783bb8f27822bd427e46495769
SuroWka-Roch <wojtek@surowka.com>
2020-03-09 15:24:37 +0100

    Todo. junky woring

--- pc_side/comunication.py
+++ pc_side/comunication.py
@@ -6,17 +6,34 @@ import time
 
 PortName = "/dev/ttyACM0"
 
+
+
+def read_chunk(ser_port):
+    try:
+        received = None
+        #handle no information relived problem
+        bytes_waiting = ser.inWaiting()
+
+        while bytes_waiting > 0:
+            received = ser.read(bytes_waiting)
+        if received:
+            return received.decode(encoding='utf-8', errors='strict')
+    except Exception as e:
+        print(str(e))
+
+
 try:
     #open port and make sure my program is running on the Arduino
-    with serial.Serial(PortName, 9600, timeout=3) as ser:
+    with serial.Serial(PortName, 9600, timeout=5) as ser:
         time.sleep(2)
-        #handle no information relived problem
-        while 1:
-            received = ser.read(1)
-            while received == b'':
-                received = ser.read(1)
-                time.sleep(0.1)
-            print(received.decode(encoding='utf-8',errors='strict'),end='')
-            
+        string = ""
+        ser.flushInput()
+        for _ in range(50):
+            time.sleep(0.1)
+            temp_string = read_chunk(ser)
+            print(temp_string)  
+            if temp_string:
+                string += temp_string
+    print(string,end="")
 except Exception as e:
     print(str(e))
