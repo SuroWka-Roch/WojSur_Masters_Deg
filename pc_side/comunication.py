35a0c02e3fdbbbddebfe442c7a3ce6b7245ab02b
SuroWka-Roch <wojtek@surowka.com>
2020-03-10 13:46:00 +0100

    Decent comunication functions.

--- pc_side/comunication.py
+++ pc_side/comunication.py
@@ -5,21 +5,61 @@ import serial
 import time
 
 PortName = "/dev/ttyACM0"
-
+SERIAL_BUFFER_SIZE = 64
+READY_TO_READ = "Ready to read\n"
+COMAND_ENDING_CONST = '\n'
+SLEEP_TIME = 0.01
+WAIT_FOR = 1000
 
 
 def read_chunk(ser_port):
     try:
         received = None
         #handle no information relived problem
-        bytes_waiting = ser.inWaiting()
 
-        while bytes_waiting > 0:
-            received = ser.read(bytes_waiting)
+        while ser.inWaiting() > 0:
+            received = ser.read(ser.inWaiting())
         if received:
             return received.decode(encoding='utf-8', errors='strict')
     except Exception as e:
         print(str(e))
+
+
+def send_and_expect(serialPort, send, expect):
+    """
+        @brief send comand to controler
+        @param send string to send to controler
+        @param expect Wait for that response, set to None if no response needed.
+        @return recived information during communications.
+    """
+    temp_log = ""
+    serialPort.write(COMAND_ENDING_CONST.encode(encoding='utf-8'))
+    
+    waited_for_response = 0
+    while temp_log.find(READY_TO_READ) == -1:
+        time.sleep(SLEEP_TIME)
+        temp_string = read_chunk(serialPort)
+        if temp_string:
+                temp_log += temp_string
+        waited_for_response += 1
+        if waited_for_response > WAIT_FOR:
+            return temp_log
+
+    serialPort.write(send.encode(encoding='utf-8'))
+
+    if expect:
+        while temp_log.find(expect) == -1:
+            time.sleep(SLEEP_TIME)
+            temp_string = read_chunk(serialPort)
+            if temp_string:
+                    temp_log += temp_string
+            waited_for_response += 1
+            if waited_for_response > WAIT_FOR:
+                return temp_log
+
+
+    return temp_log
+
 
 
 try:
@@ -27,13 +67,17 @@ try:
     with serial.Serial(PortName, 9600, timeout=5) as ser:
         time.sleep(2)
         string = ""
+
+        string += send_and_expect(ser, "atm\n1000\n", "1000\n")
+        string += send_and_expect(ser, "42a\n", "42b\n")
+
+        
         ser.flushInput()
-        for _ in range(50):
-            time.sleep(0.1)
+        for _ in range(100):
+            time.sleep(0.01)
             temp_string = read_chunk(ser)
-            print(temp_string)  
             if temp_string:
                 string += temp_string
-    print(string,end="")
+    print(string, end="")
 except Exception as e:
     print(str(e))
