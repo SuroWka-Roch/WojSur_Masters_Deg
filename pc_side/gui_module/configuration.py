e1deb9d4ec8541d38c577eb30cfad710c49f1b04
SuroWka-Roch <wojtek@surowka.com>
2020-06-26 12:15:22 +0200

    Update gui

--- pc_side/gui_module/configuration.py
+++ pc_side/gui_module/configuration.py
@@ -3,4 +3,4 @@ Header like file with all static defines.
 """
 
 CANAL_NAMES = ["1A1","1A2","1A3","1A4","1A5","1A6","1A7","1A8","2A1","2A2","2A3","2A4","2A5","2A6","2A7","2A8"]
-GUI_UPDATE_PERIOD_MS = 1000
+GUI_UPDATE_PERIOD_MS = 100
